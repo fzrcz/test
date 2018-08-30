d7d8818fef24d0ea0f46d4e80f22dbc077dce3f7
fzrcz <31850692+fzrcz@users.noreply.github.com>
2018-08-30 15:09:38 +0800

    增加算术问题

--- MainStart.java
+++ MainStart.java
@@ -2,4 +2,23 @@ public class Rcz{
     public static void main(String[] args){
        System.out.println("hello git");
     }
+    
+    public int add(int a,int b){
+        return a+b;
+    }
+    
+    public int sub(int a,int b){
+        return a-b;
+    }
+    
+    public int multi(int a,int b){
+        return a*b;
+    }
+    
+    public int div(int a,int b){
+        if(b==0){
+            throw new Exception("除数不能为0"):
+        }
+        return a/b;
+    }
 }
